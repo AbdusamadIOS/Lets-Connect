c014f12bf82277201b0d9b9fb1a6fee401ad4db3
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-08-07 15:08:14 +0500

    Initial Commit

---
+++ Lets Connect/CircleEmitter.swift
@@ -0,0 +1,23 @@
+
+import SwiftUI
+
+struct CircleEmitter: View {
+    @Binding var isAnimating: Bool
+    
+    var body: some View {
+        Circles()
+            .fill(Color.white.opacity(0.75))
+            .scaleEffect(isAnimating ? 2 : 0)
+            .animation(isAnimating ? Animation.spring() : Animation.easeInOut(duration: 0), value: isAnimating)
+    }
+}
+
+struct CircleEmitter_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            CircleEmitter(isAnimating: .constant(true))
+        }
+            
+    }
+}
---
+++ Lets Connect/Lets_ConnectApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct Lets_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
